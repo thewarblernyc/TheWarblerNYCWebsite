4440795c2912d8cd963a678cce186b85289bbe0b
Richard So <rs21@pop-os.localdomain>
2020-09-19 22:30:04 -0400

    fix for heroku

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-import json
+# import json
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-publications = json.load("data/publication.json")
+# publications = json.load("data/publication.json")
 
 @app.route("/")
 def home():
